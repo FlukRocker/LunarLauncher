138e16bc0aab9fc35179fcee29559f6b9e403377
Aukrit Kantawong <flukrocker@outlook.co.th>
2023-12-16 23:36:53 +0700

    remove jvmOptions

--- app/assets/js/configmanager.js
+++ app/assets/js/configmanager.js
@@ -552,14 +552,7 @@ function defaultJavaConfig17(ram) {
         minRAM: resolveSelectedRAM(ram),
         maxRAM: resolveSelectedRAM(ram),
         executable: null,
-        jvmOptions: [
-            '-XX:+UnlockExperimentalVMOptions',
-            '-XX:+UseG1GC',
-            '-XX:G1NewSizePercent=20',
-            '-XX:G1ReservePercent=20',
-            '-XX:MaxGCPauseMillis=50',
-            '-XX:G1HeapRegionSize=32M'
-        ],
+        jvmOptions: [],
     }
 }
 
